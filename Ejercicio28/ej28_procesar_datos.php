d02ab93b45b99f791e4e0a81642fbe85fb0f952f
BlackCherryCat <blackcherry@danwin1210.de>
2024-11-26 21:13:59 +0100

    fix ej28_procesar_datos

--- Ejercicio28/ej28_procesar_datos.php
+++ Ejercicio28/ej28_procesar_datos.php
@@ -2,7 +2,7 @@
 session_start();
 $valor = $_POST["name"];
 setcookie("nombre_usu", $valor, time() + (3600 * 24));
-$_SESSION["mail"] = $_POST["mail"];
-$_SESSION["color"] = $_POST["color"];
+$_SESSION["mail"] = htmlspecialchars($_POST["mail"]);
+$_SESSION["color"] = htmlspecialchars($_POST["color"]);
 header("Location: ej28_bienvenida.php");
 exit();
